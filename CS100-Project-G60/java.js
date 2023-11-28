6bb8af1833d76fcf65abb00a074551d1884f1e62
Thanakrit Piboonsawat - 6609650400 <145310502+Thanakritpiboonsawat@users.noreply.github.com>
2023-11-28 22:24:27 +0700

    Add files via upload

---
+++ CS100-Project-G60/java.js
@@ -0,0 +1,7 @@
+document.getElementById("surveyForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    // You can add additional validation or processing here
+
+    alert("Form submitted!");
+});

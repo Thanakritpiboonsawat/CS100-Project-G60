20f3a32f358b2d4f12f06a9228641f814a137e33
Peemapat Jiravatvanich-6609650566 <145252834+Xagatech@users.noreply.github.com>
2023-12-10 17:51:27 +0700

    Delete CS100-Project-G60 directory

--- CS100-Project-G60/java.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("surveyForm").addEventListener("submit", function(event) {
-    event.preventDefault();
-
-    // You can add additional validation or processing here
-
-    alert("Form submitted!");
-});
